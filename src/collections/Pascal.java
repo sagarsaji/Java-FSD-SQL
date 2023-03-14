317bb9a1138f44c5d8d1ebf71d27ef6ff3bb62f6
Sagar Saji <sagarsaji7@gmail.com>
2023-03-14 22:17:44 +0530

    22.17

--- src/collections/Pascal.java
+++ src/collections/Pascal.java
@@ -7,11 +7,14 @@ public class Pascal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int lim;
-		ArrayList<>
+		//ArrayList<>
 		Scanner n = new Scanner(System.in);
 		System.out.println("Enter limit of triangle : ");
 		lim = n.nextInt();
-		for(int i=0;i<lim;i++) {
+		for(int i=0;i<1;i++) {
+			for(int j=0;j<2;j++) {
+				System.out.println(1);
+			}
 			
 		}
 	}
